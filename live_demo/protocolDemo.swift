0ada8eafaa04bf5b8d91045edf1ebe045be9213b
rafaelmbarroso <rafaelmbarroso@outlook.com>
2026-04-28 01:29:54 -0500

    simplified code in protocol demo

--- live_demo/protocolDemo.swift
+++ live_demo/protocolDemo.swift
@@ -1,19 +1,31 @@
-protocol StudentProto {
+// Define a protocol for students
+protocol UniversityProto {
     var name: String { get set }
     var age: Int { get set }
     var id: Int { get set }
     var grade: String { get set }
 }
 
-struct SomeStudent: StudentProto {
+// Define the struct for the students that conforms to the protocol
+struct SomeStudent: UniversityProto {
     var name: String
     var age: Int
     var id: Int
     var grade: String
 }
 
+// Note how the faculty struct also conforms to the same protocol, but has an additional property for department
+struct SomeFaculty: UniversityProto {
+    var name: String
+    var age: Int
+    var id: Int
+    var grade: String
+    var department: String
+}
+
 let student1 = SomeStudent(name: "John", age: 20, id: 12345, grade: "A")
-print("Student name: \(student1.name)") // Output: John
-print("Student age: \(student1.age)")  // Output: 20
-print("Student ID: \(student1.id)")   // Output: 12345
-print("Student grade: \(student1.grade)") // Output: A
+let faculty1 = SomeFaculty(name: "Dr. Smith", age: 45, id: 54321, grade: "", department: "Computer Science")
+
+print("Student name: \(student1.name)\nStudent age: \(student1.age)\nStudent ID: \(student1.id)\nStudent grade: \(student1.grade)")
+print("\n")
+print("Faculty name: \(faculty1.name)\nFaculty age: \(faculty1.age)\nFaculty ID: \(faculty1.id)\nDepartment: \(faculty1.department)")
